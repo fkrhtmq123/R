6c9b8e65823b848b8fa29eff8737d10c703450e3
fkrhtmq123 <fkrhtmq123@naver.com>
2020-08-03 17:12:29 +0900

    2020/08/03 Ch01 등록

---
+++ Ch01/1-2.R
@@ -0,0 +1,30 @@
+#날짜 : 2020/08/03
+#이름 : 김동욱
+#내용 : 교재 p42 변수와 자료형
+
+#숫자
+var1 <- 1
+var2 <- 2
+var3 <- 3.14
+
+#논리형
+var4 <- TRUE
+var5 <- FALSE
+
+#문자형
+var6 <- "Hello"
+var7 <- '안녕'
+
+#결측값
+var8 <- NA      #Not Available
+var9 <- NaN     #Not a Number
+
+var1
+var2
+var3
+var4
+var5
+print(var6)
+print(var7)
+print(var8)
+print(var9)
