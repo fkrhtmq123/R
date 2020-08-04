e8dc722c39e4dc39a626227d2cc1a383cd3ea3b7
fkrhtmq123 <fkrhtmq123@naver.com>
2020-08-04 16:08:02 +0900

    dplyr 실습

--- Ch03/3-4.R
+++ Ch03/3-4.R
@@ -1,6 +1,6 @@
 #날짜 : 2020/08/04
 #이름 : 김동욱
-#내용 : 교재 p176 dplyr 패키지 실습하기기
+#내용 : 교재 p176 dplyr 패키지 실습하기
 
 #패키지설치
 install.packages("dplyr")
@@ -8,7 +8,7 @@ library(dplyr)
 
 df_exam <- read.csv('../file/exam.csv')
 
-#select
+#select - DF의 특정 컬럼을 선택해서 새로운 DF생성
 df_math    <- df_exam %>% select(math)
 df_english <- df_exam %>% select(english)
 df1        <- df_exam %>% select(math, science)
@@ -19,9 +19,67 @@ df_english
 df1
 df2
 
-#filter
-#arrange
-#mutate
-#summarise
-#group_by
-#left_join
+#filter - DF의 특정 조건에 맞는 컬럼 선택해서 새로운 DF생성
+df_class1 <- df_exam %>% filter(class==1)
+df_class1
+
+#영어 점수 60점이상 그리고 80미만
+df_english <- df_exam %>% filter(english >= 60 & english < 80)
+df_english
+
+#과학 점수 60점이상 또는 80이하
+df_science <- df_exam %>% filter(science >= 60 & science <= 80)
+df_science
+
+#SELECT math FROM df_exam WHERE class=1
+df_math <- df_exam %>% filter(class==1) %>% select(math)
+df_math
+
+#SELECT * FROM df_exam WHERE math > 60 AND math < 80
+df_all <- df_exam %>% select(everything()) %>% filter(math > 60 & math < 80)
+df_all
+
+#arrange - DF의 특정 컬럼을 정렬해서 새로운 DF생성
+df_math_asc <- df_exam %>% arrange(math)
+df_math_desc <- df_exam %>% arrange(desc(math))
+df_math_asc
+df_math_desc
+
+#SELECT class, id, math FROM df_exam WHERE math >= 60 ORDER BY math DESC LIMIT 3;
+df_result <- df_exam %>% filter(math >= 60) %>% 
+                         select(class, id, math) %>% 
+                         arrange(desc(math)) %>% 
+                         head(3)
+df_result #filter 랑 select의 자리 바꿔도 무방(일반 쿼리문과 거의 동일)
+
+#mutate - DF의 새로운 특정 컬럼(파생변수수)을 추가해서 새로운 DF생성
+df_total <- df_exam %>% mutate(total = math+english+science)
+df_total
+
+df_mean <- df_total %>% mutate(mean = total / 3)
+df_mean
+
+df_grade <- df_mean %>% mutate(grade = ifelse(df_mean$mean >= 90, 'A', 
+                                       ifelse(df_mean$mean >= 80, 'B', 
+                                       ifelse(df_mean$mean >= 70, 'C', 
+                                       ifelse(df_mean$mean >= 60, 'D', 'F')))))
+df_grade
+
+#summarise - DF의 특정 컬럼에 대한 요약 통계
+df_exam %>% summarise(mean_math=mean(math))
+df_exam %>% summarise(sum_math=sum(math))
+
+#group_by - DF의 특정 컬럼에 대해 그룹화
+df_group1 <- df_exam %>% group_by(class) %>% summarise(sum_math=sum(math))
+df_group1
+
+df_group2 <- df_exam %>% group_by(class) %>% summarise(mean_math=mean(math))
+df_group2
+
+#inner_join - 두 개 이상의 DF의 공통 컬럼으로 조인
+df_teacher <- data.frame(class=c(1, 2, 3, 4, 5),
+                         teacher=c('김유신', '김춘추', '장보고', '강감찬', '이순신'))
+df_teacher
+
+df_join <- inner_join(df_exam, df_teacher, by='class')
+df_join
